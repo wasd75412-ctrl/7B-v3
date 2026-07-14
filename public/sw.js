22331f947aab8dedfe4eef59cd1e6403c6ea14ea
wasd75412-ctrl <wasd75412@gmail.com>
2026-07-14 10:57:37 +0800

    Expand and randomize scoreboard themes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='7b-bcm-20260714-score-themes-263';
+const CACHE='7b-bcm-20260714-random-themes-264';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png','./assets/7b-logo-full.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
